4d6c4b9ff7dd4eed7564c822ad1289c499827c2a
Fireball19 <fireball1199@googlemail.com>
2022-11-23 15:58:17 +0100

    Add load scenes and exit button functionality

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -20,5 +24,17 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit() {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else    
+            Application.Quit();
+        #endif    
     }
 }
